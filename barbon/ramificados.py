bdd53de460e09700e27dc8cf517ce2e97e24ea60
Elias Auque <auque.elias@gmail.com>
2022-02-22 18:44:19 -0500

    platzi barbon

---
+++ barbon/ramificados.py
@@ -0,0 +1,9 @@
+n1= int(input("Ingrese un numero: "))
+n2= int(input("Ingrese otro numero: "))
+
+if n1 >n2:
+    print("El primer numero es mayor")
+elif n1<n2:
+    print("El segundo numero es mayor")
+else:
+    print("Son iguales")
